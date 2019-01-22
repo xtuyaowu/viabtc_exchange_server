7ca75f05c83affa95b633c0d1e39983148c7ff3f
yaowu <toyaowu@163.com>
2019-01-22 14:43:23 +0800

    cancel institute order do not send kafka message

--- matchengine/me_server.c
+++ matchengine/me_server.c
@@ -658,7 +658,7 @@ static int on_cmd_order_cancel(nw_ses *ses, rpc_pkg *pkg, json_t *params)
             while ((node = skiplist_next(iter)) != NULL) {
                 index++;
                 order_t *order = node->value;
-                int ret = market_cancel_order(true, &result, market, order);
+                int ret = market_cancel_order(false, &result, market, order);
                 if (ret < 0) {
                     log_fatal("cancel order: %"PRIu64" fail: %d", order->id, ret);
                     return reply_error_internal_error(ses, pkg);
